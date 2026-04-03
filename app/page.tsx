1166caf64a77de36a2aae5b6e5fe85aaed43a70b
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-04-03 12:57:00 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 'use client';
 
+import dynamic from 'next/dynamic';
+
+const AppShell = dynamic(() => import('../AppShell'), { 
+  ssr: false,
+  loading: () => <div style={{color:'white',padding:20}}>جاري التحميل...</div>
+});
+
 export default function Home() {
-  return <div style={{color:'white',padding:20}}>يعمل!</div>;
+  return <AppShell />;
 }
