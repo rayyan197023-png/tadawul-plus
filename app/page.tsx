a2200f4d4364e2b63e6b0709054e1ffcc678cb2f
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-04-03 13:35:16 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import { useEffect } from 'react';
 
 const AppShell = dynamic(() => import('../AppShell'), { 
   ssr: false,
@@ -9,11 +8,5 @@ const AppShell = dynamic(() => import('../AppShell'), {
 });
 
 export default function Home() {
-  useEffect(() => {
-    window.onerror = (msg, src, line) => {
-      document.body.innerHTML = '<div style="color:red;padding:20px;font-size:12px">' + msg + ' | ' + src + ':' + line + '</div>';
-    };
-  }, []);
-  
   return <AppShell />;
 }
