246865583a5fd87171e5ef912f60659ff4baef0e
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-04-07 01:50:16 +0300

    fix

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import dynamic from 'next/dynamic';
-const AppShell = dynamic(() => import('../AppShell'), { ssr: false });
-export default function Home() {
-  return <AppShell />;
-}
