ad8a957abe5dadae053248166e4c9859a907afc3
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-04-07 12:45:52 +0300

    fix

--- screens/AnalysisScreen.jsx
+++ screens/AnalysisScreen.jsx
@@ -6,7 +6,7 @@
  */
 
 import { useState, useMemo, memo } from 'react';
-import { useStocks }         from '../store';
+import { useStockState as useStocks }  from '../store';
 import { useNav }            from '../store';
 import { calcRadarScore }    from '../engines/radarEngine';
 import { generateOHLCBars }  from '../services/api/stocksApi';
