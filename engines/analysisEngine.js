9bcc1e3e5715b29f210123e88dcd0331d9d7cc06
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-04-09 16:12:27 +0300

    fix

--- engines/analysisEngine.js
+++ engines/analysisEngine.js
@@ -18,7 +18,7 @@
  */
 
 import { STOCKS } from '../constants/stocksData';
-import { calcRSI, calcATR } from './technicalEngine';
+import { calcRSI, calcATR, calcVWAP, calcCMF, calcOBV, calcMACD, calcMarketStructure } from './technicalEngine';
 
 
 // ── Color tokens used in analysis output (signals, labels)
