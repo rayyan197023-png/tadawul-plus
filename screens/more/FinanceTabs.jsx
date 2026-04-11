8663c74b06b96505f5f5b06a373db8c41282888e
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-04-11 16:44:45 +0300

    fix

--- screens/more/FinanceTabs.jsx
+++ screens/more/FinanceTabs.jsx
@@ -4,7 +4,7 @@
  * @description تبويبات مالية: توزيعات، اكتتابات، صناديق، تقويم، كلي
  */
 import { useState, useMemo, useEffect } from 'react';
-import { ArcRing, C, DIVS, EVENTS, FUNDS, IPOS, Ico, MiniLine, SectionHeader, Stars } from './MoreShared';
+import { ArcRing, C, DIVS, EVENTS, FUNDS, IPOS, Ico, MiniLine, SectionHeader, Stars, MACRO } from './MoreShared';
 
 function DividendsTab(props) {
   var tp=props.p?props.p:props;
