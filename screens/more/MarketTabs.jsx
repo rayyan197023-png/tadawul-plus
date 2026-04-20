2d471e1cc09b0379bd8f3c15bf35e0399846753b
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-04-20 08:09:07 +0300

    fix

--- screens/more/MarketTabs.jsx
+++ screens/more/MarketTabs.jsx
@@ -5,6 +5,7 @@
  */
 import React, { useState, useMemo, useEffect } from 'react';
 import { C, Ico, SentimentGauge, MiniLine, SparkLine, TagFilter, SectionHeader, COMM, RANKINGS } from './MoreShared';
+import { useNav } from '../../store';
 const RankItem = React.memo(function RankItem({ s, i, rField, BOX, SHD, SHD_ACTIVE }) {
   const isBig = i < 3;
   return (
