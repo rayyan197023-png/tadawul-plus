ebdfb4598836e68f6aca91133ed49b5595e64491
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-04-20 07:08:08 +0300

    fix

--- screens/more/ToolsTabs.jsx
+++ screens/more/ToolsTabs.jsx
@@ -7,6 +7,7 @@ import React, { useState, useEffect, useRef, useMemo, useCallback } from 'react'
 import { STOCKS } from '../../constants/stocksData';
 import { C, Ico, MiniLine, PRIORITY_ORDER, SectionHeader, SparkLine, TagFilter } from './MoreShared';
 import { useHaptic } from '../../hooks/useHaptic';
+import { useNav } from '../../store';
 
 function SnapshotsTab(props) {
   var tp=props.p?props.p:props;
