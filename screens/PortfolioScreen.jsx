16422c55bde97f854d2bc2ee4a00757ff371d055
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-04-21 07:30:24 +0300

    fix

--- screens/PortfolioScreen.jsx
+++ screens/PortfolioScreen.jsx
@@ -20,7 +20,7 @@
 import React, { useState, useMemo, useEffect } from 'react';
 import { useHaptic } from '../hooks/useHaptic';
 import { genBars, stockHealth } from '../engines/analysisEngine';
-import { analyzePortfolio, addIntelligenceLayer, generatePortfolioValueChart } from '../engines/portfolioEngine';
+import { analyzePortfolio, addIntelligenceLayer, generatePortfolioValueChart, generateDrawdownChart } from '../engines/portfolioEngine';
 import RiskDashboard from '../components/RiskDashboard';
 import PortfolioValueChart from '../components/charts/PortfolioValueChart';
 import DrawdownChart from '../components/charts/DrawdownChart';
