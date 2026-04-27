47a57d3d6b41128076a2fc15187e480ccfaacb9a
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-04-27 03:24:50 +0300

    fix

--- screens/AlertsScreen.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function AlertsScreen() {
-  return (
-    <div style={{padding: 20, color: '#fff', background: '#000', minHeight: '100vh'}}>
-      <h1>التنبيهات تعمل!</h1>
-    </div>
-  );
-}
