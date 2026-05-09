cbddfe664f6f20bad01d0c67b10573795e397436
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-05-09 15:09:22 +0300

    fix

--- AppShell.jsx
+++ AppShell.jsx
@@ -369,7 +369,7 @@ import('./lib/registerSW').then(({ unregisterServiceWorker }) => {
         width: '100%',
         maxWidth: 480,
         zIndex: 50,
-        display: snapOpen ? 'none' : 'block',
+        display: 'block',
       }}>
         <TadawulNav />
       </div>
