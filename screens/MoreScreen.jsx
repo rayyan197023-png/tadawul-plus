a6cf2057aec5e0a81e8886394506a3518bec7f69
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-05-09 04:44:48 +0300

    fix

--- screens/MoreScreen.jsx
+++ screens/MoreScreen.jsx
@@ -390,7 +390,7 @@ useEffect(function(){
     divCost:divCost, setDivCost:setDivCost,
     snaps: extSnaps && extSnaps.length > 0 ? extSnaps : snaps,
 setSnaps:setSnaps,
-snapOpen:snapOpen, setSnapOpen:function(v){setSnapOpen(v);try{if(setExtSnapOpen)setExtSnapOpen(v);}catch(e){}},
+snapOpen:snapOpen, setSnapOpen:setSnapOpen,
     fullSnap:fullSnap, setFullSnap:setFullSnap,
     editSnap:editSnap, setEditSnap:setEditSnap,
     snapTag:snapTag, setSnapTag:setSnapTag,
