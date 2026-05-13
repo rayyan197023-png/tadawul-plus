b53e1cc24fa09c8d79a7befc0889b03994c4189d
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-05-13 09:03:05 +0300

    fix

--- hooks/useLiveStockPrices.js
+++ hooks/useLiveStockPrices.js
@@ -13,6 +13,7 @@
 
 import { useEffect, useRef, useCallback } from 'react';
 import config from '../constants/config';
+import { STOCKS_MAP } from '../constants/stocksData';
 import { fetchAllStocks } from '../services/api/stocksApi';
 import { useStocks } from '../store';
 
