a70eb357b4736b2dfe24f6da0afb91ff9d3f118e
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-05-13 20:28:33 +0300

    fix

--- screens/StocksScreen.jsx
+++ screens/StocksScreen.jsx
@@ -3,6 +3,7 @@ import React, { useState, useMemo, useEffect, useRef, useCallback, startTransiti
 import { useHaptic }          from '../hooks/useHaptic';
 import { usePullToRefresh }   from '../hooks/usePullToRefresh';
 import { useNav, useSharedPrices, useStockState } from '../store';
+import { STOCKS } from '../constants/stocksData';
 import config from '../constants/config';
 import StockDetail from '../features/stock/StockDetail';
 
