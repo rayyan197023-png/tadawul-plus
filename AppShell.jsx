d61165f0aafaaa318092ccc622ae3a5a3e56e912
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-05-14 00:37:38 +0300

    fix

--- AppShell.jsx
+++ AppShell.jsx
@@ -7,6 +7,7 @@ import TadawulNav from './components/shared/TadawulNav';
 import { TABS, TAB_IDS } from './constants/navigation';
 import ErrorBoundary from './components/ErrorBoundary';
 import { usePriceUpdater } from './hooks/usePriceUpdater';
+import { useLiveStockPrices } from './hooks/useLiveStockPrices';
 import { useMarketBridge } from './hooks/useMarketBridge';
 import { useHaptic } from './hooks/useHaptic';
 import { colors } from './theme/tokens';
