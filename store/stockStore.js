af6ded0c2d6b7beee210accfdf762cb300aa0f13
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-05-15 11:01:01 +0300

    fix

--- store/stockStore.js
+++ store/stockStore.js
@@ -70,12 +70,8 @@ function stockReducer(state, action) {
   switch (action.type) {
 
     case STOCK_ACTIONS.SET_STOCKS: {
-  try {
-    localStorage.setItem('td_stocks_cache_v1', JSON.stringify(action.payload));
-  } catch {}
   return { ...state, stocks: action.payload };
 }
-
     case STOCK_ACTIONS.UPDATE_PRICE: {
       const { sym, data } = action.payload;
       return {
