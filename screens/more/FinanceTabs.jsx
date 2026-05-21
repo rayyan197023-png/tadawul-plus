de2315d1dff635c5a48ff1939430d62732daa3b4
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-05-21 15:38:25 +0300

    fix

--- screens/more/FinanceTabs.jsx
+++ screens/more/FinanceTabs.jsx
@@ -6,8 +6,6 @@
 import { useState, useMemo, useEffect } from 'react';
 import { STOCKS_LIVE as STOCKS } from '../../constants/stocksData';
 import { ArcRing, C, DIVS, EVENTS, FUNDS, IPOS, Ico, MiniLine, SectionHeader, Stars, MACRO } from './MoreShared';
-import { ArcRing, C, DIVS, EVENTS, FUNDS, IPOS, Ico, MiniLine, SectionHeader, Stars, MACRO } from './MoreShared';
-import { DIV_STOCKS } from '../../constants/dividendStocks';
 import { DIV_STOCKS } from '../../constants/dividendStocks';
 import { useDividendsList } from '../../hooks/useDividend';
 
