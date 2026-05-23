4be6268feba2050c660509a4258ad57a23812030
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-05-23 16:17:43 +0300

    fix

--- screens/MoreScreen.jsx
+++ screens/MoreScreen.jsx
@@ -17,7 +17,6 @@ import { useSharedPrices } from '../store';
 import { useHaptic } from '../hooks/useHaptic';
 import AILearningDashboard from '../components/AILearningDashboard';
 import { usePullToRefresh } from '../hooks/usePullToRefresh';
-import { usePullToRefresh } from '../hooks/usePullToRefresh';
 import { useRankings } from '../hooks/useRankings';
 // ── Shared constants + UI
 import { C, Ico, COMM, PRIORITY_ORDER, WATCHLIST_DEFAULT, RANKINGS, FUNDS, IPOS, EVENTS, DIVS, CSS_STR, TADAWUL, WTI } from './more/MoreShared'; 
