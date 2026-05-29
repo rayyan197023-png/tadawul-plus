86faa7700cb6d07d084d8c25d070355983be3ca4
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-05-29 17:46:39 +0300

    fix

--- screens/BacktestScreen.jsx
+++ screens/BacktestScreen.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import { stockHealth } from '../engines/analysisEngine';
+import { stockHealth, analyzeStockRadar } from '../engines/analysisEngine';
 import { calcRSI, calcOBV, calcCMF, calcATR } from '../engines/technicalEngine';
 import { 
   backtest, 
