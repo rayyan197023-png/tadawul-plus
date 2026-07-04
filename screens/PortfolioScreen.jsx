2aff8d3615666821d765c0d4bd32c883ca4ae979
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-07-04 12:22:23 +0300

    fix

--- screens/PortfolioScreen.jsx
+++ screens/PortfolioScreen.jsx
@@ -1297,8 +1297,6 @@ useEffect(() => {
         
 var smartBars = hasRealData ? realBars : [];
       var smartAction = null;
-      var completedTargets = completedTargetsMap[pp.sym] || [];
-      alert('map='+JSON.stringify(completedTargetsMap)+' sym='+pp.sym);
       try {
         if(h && smartBars && smartBars.length >= 14) {
 
