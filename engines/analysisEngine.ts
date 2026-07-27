3ed5d95e9c2f58009db442b2ff11c94d7d21f784
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-07-27 20:56:45 +0300

    fix

--- engines/analysisEngine.ts
+++ engines/analysisEngine.ts
@@ -20,6 +20,7 @@
 import { STOCKS_LIVE as STOCKS } from '../constants/stocksData';
 import { GATE_THRESHOLDS as GATE_THRESHOLDS_CONFIG } from '../constants/analysisConstants';
 import { GRADE_THRESHOLDS } from '../constants/analysisConstants';
+import { RISK } from '../constants/analysisConstants';
 import { calcRSI, calcATR, calcVWAP, calcCMF, calcOBV, calcMACD, calcMarketStructure, calcIVWAP } from './technicalEngine';
 import { calcOrderBlocks, calcLiqSweep } from './radarEngine';
 
