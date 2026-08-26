e35574907edc8eafd63ba80b30d409b34bd24c3e
rayyan197023-png <268761880+rayyan197023-png@users.noreply.github.com>
2026-08-27 00:57:10 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -53,7 +53,7 @@ const nextConfig = {
           { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=(self), payment=()' },
           { key: 'X-DNS-Prefetch-Control', value: 'on' },
           { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
-{ key: 'Content-Security-Policy', value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https:; connect-src 'self' https://*.supabase.co https://eodhd.com https://app.sahmk.sa https://*.sahmk.sa; frame-src 'self'; object-src 'none';" },
+{ key: 'Content-Security-Policy', value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https:; connect-src 'self' https://*.supabase.co  https://app.sahmk.sa https://*.sahmk.sa; frame-src 'self'; object-src 'none';" },
         ],
       },
       {
